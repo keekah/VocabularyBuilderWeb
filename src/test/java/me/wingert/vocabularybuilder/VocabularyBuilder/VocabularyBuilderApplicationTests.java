c93ac1ab9398daf564f236f0236c9c6b3bf8da01
keekah <kika.wingert@gmail.com>
2021-03-11 17:21:55 -0800

    initial commit

---
+++ src/test/java/me/wingert/vocabularybuilder/VocabularyBuilder/VocabularyBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package me.wingert.vocabularybuilder.VocabularyBuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocabularyBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
