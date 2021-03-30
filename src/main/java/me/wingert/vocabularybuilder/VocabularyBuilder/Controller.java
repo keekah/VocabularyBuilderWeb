ca9fef88d530a016c4a6da4f0195145fd0f7b0bd
keekah <kika.wingert@gmail.com>
2021-03-30 13:51:01 -0700

    methods getAll, newWord, and deleteWord working

--- src/main/java/me/wingert/vocabularybuilder/VocabularyBuilder/Controller.java
+++ src/main/java/me/wingert/vocabularybuilder/VocabularyBuilder/Controller.java
@@ -1,9 +1,13 @@
 package me.wingert.vocabularybuilder.VocabularyBuilder;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -20,5 +24,57 @@ public class Controller {
     {
         return repository.findAll();
     }
+
+    @PostMapping("/new-word")
+    public VocabularyWord newWord(@RequestBody VocabularyWord newWord)
+    {
+        return repository.save(newWord);
+    }
+
+    @DeleteMapping("/vocabulary-words/{id}")
+    public void deleteWord(@PathVariable int id)
+    {
+        repository.deleteById(id);
+    }
+
+    /*********************************************************************/
+//    @GetMapping("/vocabulary-words/{id}")
+//    public VocabularyWord getWord(@PathVariable int id)
+//    {
+//        return repository.findById(id)
+//                .orElseThrow(() -> new ResourceNotFoundException(id));
+//    }
+//
+//
+//
+//    @PutMapping("/vocabulary-words/{id}")
+//    public VocabularyWord updateDefinition(@PathVariable int id, @RequestBody VocabularyWord vocabWord)
+//    {
+//        return repository.findById(id)
+//                .map(word -> {
+//                    System.out.println("Word found! " + word);
+//                    String existingDefinition = word.getDefinition();
+//
+//                    try {
+//                        String definition = URLDecoder.decode(vocabWord.getDefinition(), StandardCharsets.UTF_8.name());
+//                        System.out.println("existing definition: " + existingDefinition);
+//                        if (existingDefinition == null)
+//                            word.setDefinition(definition);
+//                        else
+//                            word.setDefinition(existingDefinition + "; " + definition);
+//                    }
+//                    catch (UnsupportedEncodingException e)
+//                    {
+//                        System.out.println("Error decoding definition");
+//                        e.printStackTrace();
+//                    }
+//
+//                    VocabularyWord updatedWord = repository.save(word);
+//                    System.out.println("Saving new word " + updatedWord);
+//                    return updatedWord;
+//                })
+//                .orElseThrow(() -> new ResourceNotFoundException(id));
+//
+//    }
 
 }
--- src/main/java/me/wingert/vocabularybuilder/VocabularyBuilder/VocabWordRepository.java
+++ src/main/java/me/wingert/vocabularybuilder/VocabularyBuilder/VocabWordRepository.java
@@ -1,5 +1,10 @@
 package me.wingert.vocabularybuilder.VocabularyBuilder;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface VocabWordRepository extends JpaRepository<VocabularyWord, Integer> { }
+public interface VocabWordRepository extends JpaRepository<VocabularyWord, Integer> {
+
+//    @Query(value = "SELECT * FROM vocabulary_words WHERE word = :word", nativeQuery = true)
+//    VocabularyWord findWord(String word);
+}
